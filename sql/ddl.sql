f298a3b93923f214d5c9ed35b5b34448f9b71c84
amk244 <amk244@pitt.edu>
2019-04-07 15:31:33 -0500

    ER Changes

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,68 +1,93 @@
+--DATA DEFINITION LANGUAGE FILE
+
+DROP TABLE IF EXISTS station CASCADE;
 CREATE TABLE station(
-	station_number INTEGER NOT NULL,
-	hours_of_operation INTEGER,
-	Address VARCHAR(100),
+	station_id INTEGER NOT NULL,
+	start_time TIME NOT NULL,
+	end_time TIME NOT NULL,
+	street VARCHAR(100) NOT NULL,
+	town VARCHAR(100) NOT NULL,
+	postal_code VARCHAR(20) NOT NULL,
 	CONSTRAINT station_pk
-		PRIMARY KEY(station_number)
+		PRIMARY KEY(station_id)
 );
 
-CREATE TABLE railline
+DROP TABLE IF EXISTS rail_line CASCADE;
+CREATE TABLE rail_line
 (
-	speed_limit INTEGER,
-	distance_btwn_stations REAL,
-	station_a INTEGER NOT NULL,
-	station_b INTEGER NOT NULL,
-	CONSTRAINT railline_pk
-		PRIMARY KEY(station_a, station b),
-	CONSTRAINT railline_fk_a
-		FOREIGN KEY(station_a) REFERENCES station(station_number),
-	CONSTRAINT railline_fk_b
-		FOREIGN KEY(station_b) REFERENCES station(station_number)
+  rail_line_id INTEGER NOT NULL,
+	speed_limit INTEGER NOT NULL,
+	CONSTRAINT rail_line_pk
+		PRIMARY KEY(rail_line_id)
 );
 
+DROP TABLE IF EXISTS route CASCADE;
 CREATE TABLE route
 (
-	starting_rail_line_station INTEGER NOT NULL,
-	ending_rail_line_station INTEGER NOT NULL,
+  route_id INTEGER NOT NULL,
 	CONSTRAINT route_pk
-		PRIMARY KEY(starting_rail_line_station, ending_rail_line_station),
-	CONSTRAINT route_fk_a
-		FOREIGN KEY(starting_rail_line_station) REFERENCES station(station_number),
-	CONSTRAINT route_fk_b
-		FOREIGN KEY(ending_rail_line_station) REFERENCES station(station_number)
+		PRIMARY KEY(route_id)
 );
 
+DROP TABLE IF EXISTS route_stations;
+CREATE TABLE route_stations(
+	station_id int references station,
+	route_id int references route,
+	primary key(station_id, route_id)
+);
+
+DROP TABLE IF EXISTS rail_line_stations;
+CREATE TABLE rail_line_stations(
+	station_id int references station,
+	rail_line_id int references rail_line,
+	distance_from_prev_station int,
+	primary key(station_id, rail_line_id)
+);
+
+DROP TABLE IF EXISTS route_stops;
+CREATE TABLE route_stops(
+	station_id int references station,
+	route_id int references route,
+	primary key(station_id, route_id)
+);
+
+DROP TABLE IF EXISTS train CASCADE;
 CREATE TABLE train
 (
+  train_id INTEGER NOT NULL,
 	price_per_mile REAL NOT NULL,
 	num_seats INTEGER NOT NULL,
 	top_speed REAL NOT NULL,
-	CONSTRAINT 
-		PRIMARY KEY(price_per_mile, num_seats, top_speed)
+	CONSTRAINT train_pk
+		PRIMARY KEY(train_id)
 );
 
+DROP TABLE IF EXISTS schedule CASCADE;
 CREATE TABLE schedule
 (
-	routeID VARCHAR(100) NOT NULL,
+	schedule_route_id INTEGER NOT NULL,
 	day_of_week VARCHAR(100) NOT NULL,
-	times_of_day VARCHAR(100),
-	trainID VARCHAR(100),
-	CONSTRAINT 
-		PRIMARY KEY(route, day_of_week, times_of_day),
-	CONSTRAINT
-		FOREIGN KEY(trainID) REFERENCES train(price_per_mile, num_seats, top_speed),
-	CONSTRAINT
-		FOREIGN KEY(routeID) REFERENCES route(starting_rail_line_station, ending_rail_line_station)
+	time_of_day TIME NOT NULL,
+	schedule_train_id INTEGER NOT NULL,
+	CONSTRAINT schedule_pk
+		PRIMARY KEY(schedule_route_id, day_of_week, time_of_day),
+    CONSTRAINT route_num_fk
+        FOREIGN KEY(schedule_route_id) REFERENCES route(route_id),
+    CONSTRAINT schedule_train_id_fk
+        FOREIGN KEY(schedule_train_id) REFERENCES train(train_id)
 );
 
-CREATE TABLE passenger
+DROP TABLE IF EXISTS customer CASCADE;
+CREATE TABLE customer
 (
+  customer_id INTEGER NOT NULL,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
-	address VARCHAR(100),
+	street VARCHAR(100),
+	town VARCHAR(100),
+	postal_code VARCHAR(20),
 	email VARCHAR(100),
 	telephone_number INTEGER,
-	customerid INTEGER NOT NULL, 
-	CONSTRAINT 
-		PRIMARY KEY(customerid)
+	CONSTRAINT customer_pk
+		PRIMARY KEY(customer_id)
 );
