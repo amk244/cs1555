8d3aeebab88593e7202eb7a9dc35d010c8297048
amk244 <amk244@pitt.edu>
2019-04-08 15:17:03 -0500

    Data and DDL

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -31,23 +31,23 @@ CREATE TABLE route
 
 DROP TABLE IF EXISTS route_stations;
 CREATE TABLE route_stations(
+  route_id int references route,
 	station_id int references station,
-	route_id int references route,
 	primary key(station_id, route_id)
 );
 
 DROP TABLE IF EXISTS rail_line_stations;
 CREATE TABLE rail_line_stations(
-	station_id int references station,
 	rail_line_id int references rail_line,
+	station_id int references station,
 	distance_from_prev_station int,
 	primary key(station_id, rail_line_id)
 );
 
 DROP TABLE IF EXISTS route_stops;
 CREATE TABLE route_stops(
+  route_id int references route,
 	station_id int references station,
-	route_id int references route,
 	primary key(station_id, route_id)
 );
 
@@ -55,9 +55,9 @@ DROP TABLE IF EXISTS train CASCADE;
 CREATE TABLE train
 (
   train_id INTEGER NOT NULL,
-	price_per_mile REAL NOT NULL,
 	num_seats INTEGER NOT NULL,
 	top_speed REAL NOT NULL,
+	price_per_mile REAL NOT NULL,
 	CONSTRAINT train_pk
 		PRIMARY KEY(train_id)
 );
@@ -87,7 +87,7 @@ CREATE TABLE customer
 	town VARCHAR(100),
 	postal_code VARCHAR(20),
 	email VARCHAR(100),
-	telephone_number INTEGER,
+	telephone_number VARCHAR(20),
 	CONSTRAINT customer_pk
 		PRIMARY KEY(customer_id)
 );
